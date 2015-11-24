c54cf3c755996cdf20e71813bfc74f09c84c58a4
Karlee Adams <gunnell.karlee@gmail.com>
2015-11-24 10:47:43 -0700

    Adding Associations

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -12,4 +12,25 @@ class PinsController < ApplicationController
     @pin = Pin.find(params[:id])
   end
   
+  def new
+  	@pin = Pin.new
+  end
+
+  def create
+  	@pin = Pin.create(pin_params)
+  	if @pin.valid?
+  		@pin.save
+  		redirect_to @pin
+  	else
+  		@errors = @pin.errors
+  		render :new
+  	end
+  end
+
+  private
+
+  def pin_params
+  	params.require(:pin).permit(:title, :url, :slug, :text, :category_id)
+  end
+
 end
